cf2925cf8839840b7b49d047c9db9f07c0f884cc
Tim Payton <tim.payton@ft-design.org.uk>
2020-05-24 22:20:52 +0100

    simplify email component for now

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,62 +1,17 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-}));
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 const Email = () => {
-  const classes = useStyles();
   return (
     <Container maxWidth="md">
       <Typography variant="body1" gutterBottom>
-        To register your interest there is a contact form below, or emails can
-        be sent to <a href="mailto:j.newton@newtronics.co.uk?Subject=Newtronics%20Rail%20enquiry">j.newton@newtronics.co.uk</a>
+        For more information about any of the products please forward your
+        emails to
+        <a href="mailto:j.newton@newtronics.co.uk?Subject=Newtronics%20Rail%20enquiry">
+          j.newton@newtronics.co.uk
+        </a>
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        Contact
-      </Typography>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField
-          margin="normal"
-          id="email"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          margin="normal"
-          id="message"
-          label="Message"
-          type="text"
-          multiline
-          rows="10"
-          fullWidth
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          endIcon={<Icon>send</Icon>}
-        >
-          Send
-        </Button>
-      </form>
     </Container>
   );
 };
